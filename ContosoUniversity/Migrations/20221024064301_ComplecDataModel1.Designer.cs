6a896e8fa1797745cdb0e5661d56a36a4eb2a32b
stalg28 <stalg28@gmail.com>
2022-10-24 09:48:50 +0300

    complex data base

--- ContosoUniversity/Migrations/20221024064301_ComplecDataModel1.Designer.cs
+++ ContosoUniversity/Migrations/20221024064301_ComplecDataModel1.Designer.cs
@@ -4,14 +4,16 @@ using ContosoUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20221024064301_ComplecDataModel1")]
+    partial class ComplecDataModel1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
